bffcf1815e0db89cb6c1992978e9af60b3505844
nil1g18 <99904618+nil1g18@users.noreply.github.com>
2022-03-09 17:23:52 +0000

    Update DMP.ino
    ideas for controlling the movement of the robot

--- mpu6050/DMP_helper/DMP.ino
+++ mpu6050/DMP_helper/DMP.ino
@@ -31,3 +31,45 @@ void loop(){
   Serial.print("/");
 	Serial.println(ypr[2]); //Roll on X axis
 }
+
+/*
+counter1 and counter2 generate values from the motor encoders
+#define FULL_TURN 380    // Example number which the counter1 or counter 2 increments when the motor makes a full 360 degree turn
+#define WHEEL_DIAMETER 60	//(mm) including the extruding magnets, this will be used for calculating the circumference of the wheel
+
+volatile float travelled_distance[2] = {0}; // a distance for both wheels
+
+For now i'll assume theres only clockwise and anticlockwise spin (at max rate)
+
+Forward traveling in meters:
+			v 188.5mm v
+	Distance = pi*WHEEL_DAMETER * counter/FULL_TURN;
+
+THEREFORE :) a meter travel will be
+	if(Distance<1000)
+		forward_L();
+		forward_R();
+	else
+		stop_L();
+		stop_R();
+	
+	LMAO
+
+Now the center SPIN:
+
+#define ROBOT_DIAMETER 200 // (mm)
+					v 628.32 mm v
+	Circumference of robot would be pi*200
+
+	90 degrees turn clockwise would simply be:   /4 because a quarter of the circumference translates to 90 degrees of a spin
+	if(distance < pi*200/4)
+		forward_L();
+		backward_R();
+	else
+		stop_L();
+		stop_R();
+
+*/
+
+
+
